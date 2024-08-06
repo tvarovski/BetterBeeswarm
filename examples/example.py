6dbcbc872cf82facc91184b81b1bd692c64395a3
tvarovski <tvarovski1@gmail.com>
2024-08-06 14:34:03 -0500

    Update dpi in savefig calls to 200

--- examples/example.py
+++ examples/example.py
@@ -5,7 +5,7 @@ tips = sns.load_dataset("tips")
 
 plt.figure(figsize=(4, 4)) # set figure size
 sns.swarmplot(data=tips, x="size", y="total_bill", hue="size", alpha=0.7, palette='viridis')
-plt.savefig('native_beeswarm.png', dpi=300)
+plt.savefig('native_beeswarm.png', dpi=200)
 
 # Example 1
 from betterbeeswarm import Beeswarm
@@ -16,7 +16,7 @@ tips = sns.load_dataset("tips")
 
 plt.figure(figsize=(4, 4)) # set figure size
 sns.swarmplot(data=tips, x="size", y="total_bill", hue="size", alpha=0.7, palette='viridis')
-plt.savefig('betterbeeswarm.png', dpi=300)
+plt.savefig('betterbeeswarm.png', dpi=200)
 
 # Example 2
 from betterbeeswarm import Beeswarm
@@ -27,4 +27,4 @@ tips = sns.load_dataset("tips")
 sns.catplot(
     data=tips, kind="swarm", x="time", y="total_bill", hue="sex", 
     col="day", aspect=0.6, height=2.5, alpha=0.7, size=5)
-plt.savefig('betterbeeswarm_cat.png', dpi=300)
+plt.savefig('betterbeeswarm_cat.png', dpi=200)
